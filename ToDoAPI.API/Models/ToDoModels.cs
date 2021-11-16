dd852fd76a462940dfbf282600b01f22edf88f7d
LynnDCoding@outlook.com <LynnDCoding@outlook.com>
2021-11-16 11:19:57 -0600

    Initial Commit

---
+++ ToDoAPI.API/Models/ToDoModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoAPI.API.Models
+{
+    public class ToDoViewModel
+    {
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+
+        public virtual CategoryViewModel Category { get; set; }
+    }
+
+    public class CategoryViewModel
+    {
+        public int CategoryId { get; set; }
+        public int Todoid { get; set; }
+        public string Action { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
